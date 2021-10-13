9b1d55c35f2478afac841564a21631be49ffda9f
AdiDD <adi.deaconu21@gmail.com>
2021-10-13 07:02:41 +0300

    Display cart content

--- src/Codecool.CodecoolShop/wwwroot/js/HtmlFactory.js
+++ src/Codecool.CodecoolShop/wwwroot/js/HtmlFactory.js
@@ -1,15 +1,24 @@
-@{
-    ViewData["Title"] = "Home Page";
+﻿export const htmlTemplates = {
+    cartItem: 1
+};
+
+export function htmlFactory(template) {
+    switch (template) {
+        case htmlTemplates.cartItem:
+            return cartItemBuilder;
+        default:
+            console.error("Undefined template: " + template);
+            return () => {
+                return "";
+            };
+    }
 }
 
-@model List<Codecool.CodecoolShop.Models.Product>
-
-<div class="container">
-    @foreach (var element in Model)
-    {
+function cartItemBuilder(cartItem) {
+    return `
         <div class="col-lg-3 col-lg-3" style="display: inline-block; max-width: 350px; height: 350px">
             <div class="card">
-                <img src="../../img/@(element.Name).jpg" style="height: 50%; width: 50%; align-self: center; padding-top: 10px">
+                <img src="../../img/${cartItem.name}.jpg" style="height: 50%; width: 50%; align-self: center; padding-top: 10px">
 
                 <div class="card-body">
                     <h5 class="card-title text-center">
@@ -23,11 +32,9 @@
                     <p class="card-text">Supplier: <a asp-controller="Product" asp-action="GetBySupplier" asp-route-id="@element.Supplier.Id"> @element.Supplier.Name</a></p>
                     <p class="card-text text-center"><strong>Price: @element.DefaultPrice.ToString("C2")</strong></p>
                     @*<a type="button" class="btn btn-primary" style="float: bottom" asp-controller="Product" asp-action="AddToCart" asp-route-id="@element.Id">Add To Cart</a>*@
-                    <button class="btn btn-primary add-to-cart" data-id="@element.Id" data-name="@element.Name" data-price="@element.DefaultPrice">Add To Cart</button>
+                    <a href="" type="button" class="btn btn-primary add-to-cart" style="float: bottom" data-id="@element.Id" data-name="@element.Name" data-price="@element.DefaultPrice">Add To Cart</a>
                 </div>
             </div>
         </div>
-    }
-
-
-</div>
+    `;
+}
--- src/Codecool.CodecoolShop/wwwroot/js/site.js
+++ src/Codecool.CodecoolShop/wwwroot/js/site.js
@@ -1,36 +1,221 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart")
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
 
-// Write your JavaScript code.
+class LocalStorage {
+    static getCart() {
+        return localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+    }
 
-import { cartManager, updateHeader } from "./cart.js";
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
 
-$(document).ready(() => {
-    updateHeader();
+    static getProduct(id) {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        return cart.find(product => product.id == id);
+    }
 
-    $(".add-to-cart").each(function () {
-        $(this).on("click", function (event) {
-            event.preventDefault();
-            cartManager.addProductHandler(this);
+    static productIsInCart(product) {
+        let isInCart = false;
+        let cart = this.getCart();
+        cart.forEach(item => {
+            if (item.id == product.id)
+                isInCart = true;
         })
-    });
+        return isInCart;
+    }
 
-    $("#cart-link").on("click", function () {
-        var cart = localStorage.getItem("cart");
+    static addToCart(product) {
+        var cart = this.getCart();
+        var inCart = false;
 
-        $.ajax({
-            type: "POST",
-            url: "/Product/CheckoutJSON",
-            data: { "cart": cart},
-            dataType: "json",
-            success: function (response) {
-                if (response.success) {
-                    window.location.replace("/Product/Checkout");
-                }
-            },
-            error: function (response) {
-                alert("error!"); 
+        cart.forEach(item => {
+            if (item.name == product.name) {
+                this.updateQuantity(cart, product);
+                inCart = true;
             }
         });
-    })
-})
+
+        if (!inCart)
+            this.addItem(cart, product)
+    }
+
+    static addItem(cart, product) {
+        cart.push(product);
+        this.saveCart(cart);
+    }
+
+    static updateQuantity(cart, product) {
+        cart.forEach(item => {
+            if (item.name == product.name) {
+                item.quantity++;
+            }
+        });
+        this.saveCart(cart);
+    }
+
+    static decreaseQuantity(cart, product) {
+        cart.forEach(item => {
+            if (item.name == product.name) {
+                item.quantity--;
+            }
+        });
+        this.saveCart(cart);
+    }
+}
+
+
+class UI {
+    setUpAddToCartButtons() {
+        const buttons = [...document.querySelectorAll(".add-to-cart")];
+        buttons.forEach(button => {
+            button.addEventListener("click", () => this.addProductHandler(event.target))
+        });
+    }
+
+    setupApp() {
+        this.setUpAddToCartButtons();
+        this.cartLogic();
+        this.setCartValues();
+        this.populateCart();
+        cartBtn.addEventListener("click", this.showCart);
+        closeCartBtn.addEventListener("click", this.hideCart);
+    }
+
+    cartLogic() {
+        clearCartBtn.addEventListener("click", () => this.clearCart());
+
+        cartOverlay.addEventListener("click", (event) => {
+            if (event.target.classList.contains("transparentBcg"))
+                this.hideCart();
+        })
+
+        cartContent.addEventListener("click", event => {
+            if (event.target.classList.contains("remove-item")) {
+                let itemToRemove = event.target;
+                let id = itemToRemove.dataset.id;
+                this.removeItem(id);
+            } else if (event.target.classList.contains("fa-chevron-up")) {
+                var cart = LocalStorage.getCart();
+                let id = event.target.dataset.id;
+                let item = LocalStorage.getProduct(id);
+                LocalStorage.updateQuantity(cart, item)
+                this.setCartValues();
+                this.populateCart();
+            } else if (event.target.classList.contains("fa-chevron-down")) {
+                var cart = LocalStorage.getCart();
+                let id = event.target.dataset.id;
+                let item = LocalStorage.getProduct(id);
+                if (item.quantity == 1) {
+                    this.removeItem(id);
+                    this.hideCart();
+                } else {
+                    LocalStorage.decreaseQuantity(cart, item);
+                }                
+                this.setCartValues();
+                this.populateCart();
+            }
+        })
+    }
+
+    clearCart() {
+        console.log("pls fix clearing the cart");
+        localStorage.removeItem("cart");
+        this.hideCart();
+        this.populateCart();
+        this.setCartValues();
+    }
+
+    removeItem(id) {
+        var cart = LocalStorage.getCart();
+        cart = cart.filter(item => item.id != id)
+        LocalStorage.saveCart(cart);
+        this.setCartValues();
+        this.populateCart();
+    }
+
+    addProductHandler(doc) {
+        var product = {
+            "id": $(doc).data("id"),
+            "name": $(doc).data('name'),
+            "price": parseFloat($(doc).data('price')),
+            "quantity": 1
+        }
+
+        LocalStorage.addToCart(product);
+        this.populateCart();
+        this.setCartValues();
+    }
+
+    setCartValues() {
+        var cart = LocalStorage.getCart();
+        let priceTotal = 0;
+        let itemsTotal = 0;
+
+        cart.map(item => {
+            priceTotal += item.price * item.quantity;
+            itemsTotal += item.quantity;
+        });
+
+        cartTotal.innerText = parseFloat(priceTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+
+    displayCartItem(item) {
+        const div = document.createElement("div");
+        div.classList.add("cart-item");
+        div.innerHTML = `
+            <img src="../../img/${item.name}.jpg" alt="product" />
+            <div>
+                <h4>${item.name}</h4>
+                <h5>$${item.price}</h5>
+                <span class="remove-item" data-id=${item.id}>remove</span>
+            </div>
+            <div>
+                <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                <p class="item-amount">${item.quantity}</p>
+                <i class="fas fa-chevron-down" data-id=${item.id}></i>
+            </div>
+        `;
+        cartContent.appendChild(div);
+    }
+
+    showCart() {
+        cartOverlay.classList.add("transparentBcg");
+        cartDOM.classList.add("showCart");
+    }
+
+    hideCart() {
+        cartOverlay.classList.remove("transparentBcg");
+        cartDOM.classList.remove("showCart");
+    }
+
+    populateCart() {
+        cartContent.innerHTML = "";
+        var cart = LocalStorage.getCart();
+        cart.forEach(item => this.displayCartItem(item))
+    }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const ui = new UI();
+    ui.setupApp();
+});
+
+
+/*export function updateHeader() {
+    var itemCount = 0;
+    var cart = getCart();
+    cart.forEach(item => itemCount += item.quantity);
+
+    var header = document.getElementById("cart-header");
+    header.innerHTML = "Cart: " + itemCount;
+}*/
