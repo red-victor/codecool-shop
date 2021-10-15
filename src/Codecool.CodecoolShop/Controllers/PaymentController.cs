6886b2aa21dd5378d6f9a5d327081931856df3d0
red-victor <77648244+red-victor@users.noreply.github.com>
2021-10-15 16:30:40 +0300

    Move Payment Option generation to Payment Services

--- src/Codecool.CodecoolShop/Controllers/PaymentController.cs
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -1,13 +1,5 @@
-﻿using Codecool.CodecoolShop.Daos;
-using Codecool.CodecoolShop.Daos.Implementations;
-using Codecool.CodecoolShop.Models;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Stripe;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using static Codecool.CodecoolShop.Services.PaymentService;
 
 namespace Codecool.CodecoolShop.Controllers
 {
@@ -25,27 +17,9 @@ namespace Codecool.CodecoolShop.Controllers
         [HttpPost]
         public ActionResult Create(PaymentIntentCreateRequest request)
         {
-            var paymentIntents = new PaymentIntentService();
-            var amount = PaymentService.CalculateOrderAmount(request.Items);
-            // Add taxes
-            var tax = amount * 5 / 100;
-            amount += tax;
-            var shipping = 500;
-            amount += shipping;
-            var paymentIntent = paymentIntents.Create(new PaymentIntentCreateOptions
-            {
-                Amount = amount,
-                Currency = "usd",
-            });
-            return Json(new { clientSecret = paymentIntent.ClientSecret});
-        }
+            var paymentIntent = PaymentService.GeneratePaymentOptions(request);
 
-        
-        
-        public class PaymentIntentCreateRequest
-        {
-            [JsonProperty("items")]
-            public CartItem[] Items { get; set; }
+            return Json(new { clientSecret = paymentIntent.ClientSecret});
         }
     }
 }
--- src/Codecool.CodecoolShop/Services/PaymentService.cs
+++ src/Codecool.CodecoolShop/Services/PaymentService.cs
@@ -1,4 +1,6 @@
 ﻿using Codecool.CodecoolShop.Models;
+using Newtonsoft.Json;
+using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,33 @@ namespace Codecool.CodecoolShop.Services
                 sum += item.Price * item.Quantity;
 
             return (int)((Math.Round(sum * 100f) / 100f) * 100);
+        }
+
+        public PaymentIntent GeneratePaymentOptions(PaymentIntentCreateRequest request)
+        {
+            var paymentIntents = new PaymentIntentService();
+            var amount = CalculateOrderAmount(request.Items);
+
+            amount = AddTax(amount);
+
+            return paymentIntents.Create(new PaymentIntentCreateOptions
+            {
+                Amount = amount,
+                Currency = "usd",
+            });
+        }
+
+        public int AddTax(int amount, int shippingPrice = 500)
+        {
+            var tax = amount * 5 / 100;
+            amount += tax;
+            amount += shippingPrice;
+            return amount;
+        }
+        public class PaymentIntentCreateRequest
+        {
+            [JsonProperty("items")]
+            public CartItem[] Items { get; set; }
         }
     }
 }
