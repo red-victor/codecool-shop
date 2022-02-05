8fd2fefed83ca0d5a7e5e6e294a37393bf49be07
red-victor <77648244+red-victor@users.noreply.github.com>
2022-02-05 13:30:24 +0200

    Remove unused code in PaymentController

--- src/Codecool.CodecoolShop/Controllers/PaymentController.cs
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -1,40 +1,11 @@
-﻿using Codecool.CodecoolShop.Daos.Implementations;
-using Codecool.CodecoolShop.Data;
+﻿using Codecool.CodecoolShop.Data;
 using Microsoft.AspNetCore.Mvc;
-using static Codecool.CodecoolShop.Services.PaymentService;
-
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using Stripe;
 using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace Codecool.CodecoolShop.Controllers
 {
-    /*[Route("create-payment-intent")]
-    [ApiController]
-    public class PaymentController : Controller
-    {
-        private readonly IUnitOfWork _unitOfWork;
-        public Services.PaymentService PaymentService { get; set; }
-
-        public PaymentController(IUnitOfWork unitOfWork)
-        {
-            this._unitOfWork = unitOfWork;
-            PaymentService = new Services.PaymentService(_unitOfWork.Products);
-        }
-
-        [HttpPost]
-        public ActionResult Create(PaymentIntentCreateRequest request)
-        {
-            var paymentIntent = PaymentService.GeneratePaymentOptions(request);
-
-            return Json(new { clientSecret = paymentIntent.ClientSecret});
-        }
-    }*/
-
-
     public class PaymentsController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
